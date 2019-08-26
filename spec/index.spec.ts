4750056c1cfc1266c2cd3558a2b9888fad488752
Zachary Trank-Zelewicz <trank.zachary@gmail.com>
2019-08-26 14:15:19 -0400

    Fixes test

--- spec/index.spec.ts
+++ spec/index.spec.ts
@@ -1,9 +1,8 @@
 import 'reflect-metadata';
 import { Init } from '../src';
-import { StorageClient } from '../src/interfaces/Storage.Client';
 
 test('Bind', () => {
-    const uploader = Init(<StorageClient><any>{}, 'bucket');
+    const uploader = Init();
     expect(uploader).toBeDefined();
     expect(uploader.run).toBeDefined();
     expect(typeof uploader.run).toBe('function');
