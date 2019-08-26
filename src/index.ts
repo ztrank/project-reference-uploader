459381fa75eec6f4586516290360b6203834cf68
Zachary Trank-Zelewicz <trank.zachary@gmail.com>
2019-08-26 14:13:44 -0400

    Simplifies init process

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,18 @@
 import { Container } from 'inversify';
 import { Symbols } from './symbols';
 import { FileUtilImpl } from './implementations/File.Util.Impl';
-import { StorageClient } from './interfaces/Storage.Client';
 import { UploadUtilImpl } from './implementations/Upload.Util';
 import { PackageUploaderImpl } from './implementations/Package.Uploader';
 import { PackageUploader } from './interfaces/Package.Uploader';
+import { Storage } from '@google-cloud/storage';
 
 export function Init(
-    storageClient: StorageClient,
-    bucket: string
+    storageClientCertLocation: string = 'C:\\service-accounts\\azimuth-package-manager',
+    bucket: string = 'azimuth-packages'
 ): PackageUploader {
     const container = new Container();
     container.bind(Symbols.FileUtil).to(FileUtilImpl);
-    container.bind(Symbols.StorageClient).toConstantValue(storageClient);
+    container.bind(Symbols.StorageClient).toConstantValue(new Storage({keyFilename: storageClientCertLocation}));
     container.bind(Symbols.UploadUtil).to(UploadUtilImpl);
     container.bind(Symbols.StorageBucket).toConstantValue(bucket);
     container.bind('PackageUploader').to(PackageUploaderImpl);
