a7d83d24e3d338a2a547ac9bd9d8f7908098693f
Zachary Trank-Zelewicz <trank.zachary@gmail.com>
2019-08-27 08:52:05 -0400

    adds a process exit call

--- bin/index.js
+++ bin/index.js
@@ -5,4 +5,10 @@ var dist = require('../dist/index.js');
 var args = argv.option(dist.Options).run();
 dist.Init(args.account, args.bucket)
     .run()
-    .subscribe(() => {});
+    .subscribe({
+        next: () => {},
+        error: err => conole.error(err),
+        complete: () => {
+            process.exit(0);
+        }
+    });
